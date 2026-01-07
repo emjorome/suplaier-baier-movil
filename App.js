436e6bcbaef7d4d5a15597bea13461343675267b
Andrés Bohórquez <andbvill@espol.edu.ec>
2026-01-06 22:28:10 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,32 +1,35 @@
-import { NativeRouter } from 'react-router-native';
-import Main from './Main.jsx'
-import { AuthProvider } from './src/auth/context/AuthProvider.jsx';
-import { RewardProvider } from './src/context/RewardContext.jsx';
-import { registerForPushNotificationsAsync, sendTokenToServer } from './src/utils/notifications.js'; 
-import { useEffect } from 'react';
-import { LogBox } from "react-native"
+import { NativeRouter } from "react-router-native";
+import Main from "./Main.jsx";
+import { AuthProvider } from "./src/auth/context/AuthProvider.jsx";
+import { RewardProvider } from "./src/context/RewardContext.jsx";
+import RecompensasDataProvider from "./src/hooks/RecompensasDataProvider.jsx";
+import DescuentosDataProvider from "./src/compradores/hooks/DescuentosDataProvider.jsx";
+import { registerForPushNotificationsAsync, sendTokenToServer } from "./src/utils/notifications.js";
+import { useEffect } from "react";
+import { LogBox } from "react-native";
 
-LogBox.ignoreAllLogs(true)
+LogBox.ignoreAllLogs(true);
+
 export default function App() {
   useEffect(() => {
     registerForPushNotificationsAsync()
-      .then(token => {
-        // Enviar token al servidor
-        sendTokenToServer(token);
-      })
-      .catch(error => {
-        console.error("Error obteniendo el token de notificación: ", error);
-      });
+      .then((token) => sendTokenToServer(token))
+      .catch((error) =>
+        console.error("Error obteniendo el token de notificación: ", error)
+      );
   }, []);
-  return ( 
-  <AuthProvider>
-    <RewardProvider>
-      <NativeRouter>
-        <Main />
-      </NativeRouter>
-    </RewardProvider>
-  </AuthProvider>
+
+  return (
+    <AuthProvider>
+      <RewardProvider>
+        <RecompensasDataProvider>
+          <DescuentosDataProvider>
+            <NativeRouter>
+              <Main />
+            </NativeRouter>
+          </DescuentosDataProvider>
+        </RecompensasDataProvider>
+      </RewardProvider>
+    </AuthProvider>
   );
 }
-
-
